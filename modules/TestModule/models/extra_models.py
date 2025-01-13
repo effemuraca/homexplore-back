654acea77a7ade841808823b9f4d9bd5d47b0ae3
VinStan1 <carlovstan@gmail.com>
2025-01-13 13:40:29 +0100

    🔨Init Structure: modules

---
+++ modules/TestModule/models/extra_models.py
@@ -0,0 +1,2 @@
+# purpose:
+#     this file contains extra models, if needed, that are created combining different entity models.
---
+++ modules/TestModule/models/response_models.py
@@ -0,0 +1,2 @@
+# purpose:
+#     this file contains the response models for this module, that are also shown in the API documentation.
