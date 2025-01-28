378b3e33521fc2f580cc581f91abcb1bfdfbd39f
VinStan1 <carlovstan@gmail.com>
2025-01-28 18:03:39 +0100

    🔧main fixed

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     load_dotenv()
     logger.info("Starting the FastAPI application.")
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True, timeout_keep_alive=3600)
