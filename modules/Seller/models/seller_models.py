f5ec55d027977c374c37cde482315d0e2cc00244
VinStan1 <carlovstan@gmail.com>
2025-02-05 10:35:55 +0100

    🔧Removed create seller models

--- modules/Seller/models/seller_models.py
+++ modules/Seller/models/seller_models.py
@@ -5,27 +5,11 @@ import re
 from datetime import datetime
 
 # Seller
-class CreateSeller(BaseModel):
-    agency_name: str = Field(example="Agency Name")
-    email: str = Field(example="email@example.com")
-    password: str = Field(example="password123")
-    phone_number: str = Field(example="+38 1234567890")
-    
-    @validator("email")
-    def validate_email(cls, value):
-        email_pattern = re.compile(
-            r'^[\w\.-]+@[\w\.-]+\.\w{2,}$'
-        )
-        if not email_pattern.match(value):
-            raise ValueError("Invalid email format. Expected format: email@example.com")
-        return value
-
-    @validator("phone_number")
-    def validate_phone_number(cls, value):
-        phone_pattern = re.compile(r"^\+\d{2} \d{10}$")
-        if not phone_pattern.match(value):
-            raise ValueError("Invalid phone number format. Expected format: +xx xxxxxxxxxx")
-        return value
+# class CreateSeller(BaseModel):
+#     agency_name: str = Field(example="Agency Name")
+#     email: str = Field(example="email@example.com")
+#     password: str = Field(example="password123")
+#     phone_number: str = Field(example="+38 1234567890")
 
 class UpdateSeller(BaseModel):
     seller_id: str = Field(..., example="5f4f4f4f4f4f4f4f4f4f4f4f")
