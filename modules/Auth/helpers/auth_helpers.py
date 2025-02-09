995e067a01059c8b0c5465abbe22c209ac09bdcd
VinStan1 <carlovstan@gmail.com>
2025-02-09 20:08:59 +0100

    🔧Removed files auth

--- modules/Auth/helpers/auth_helpers.py
+++ modules/Auth/helpers/auth_helpers.py
@@ -1,30 +1,5 @@
-from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.responses import RedirectResponse
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from datetime import datetime, timedelta
-from jose import jwt
 from passlib.context import CryptContext
-
-from typing import Union
 from bcrypt import hashpw, gensalt, checkpw
-from .JwtHandler import JWTHandler
-
-# def get_user_by_access_token(access_token: str) -> Union[User, None]:
-#     """
-#     Get the user from the access token.
-    
-#     Args:
-#         access_token (str): The access token to get the user from.
-    
-#     Returns:
-#         Union[User, None]: The user if the access token is valid, None otherwise.
-#     """
-#     user_id = JWTHandler.verifyAccessToken(access_token)
-#     user = get_user_by_id(int(user_id))
-#     if(user):
-#         return user
-#     else:
-#         return None
             
 def hash_password(password: str) -> str:
     return hashpw(password.encode('utf-8'), gensalt()).decode('utf-8')
