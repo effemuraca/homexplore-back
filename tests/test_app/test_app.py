9f42668d800655a94ce67e11483c455798f4b988
VinStan1 <carlovstan@gmail.com>
2025-02-14 15:37:33 +0100

    🔧removed

--- tests/test_app/test_app.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-
-from app import app
-
-client = TestClient(app)
-
-def test_main():
-    response = client.get("/")
-    assert response.status_code == 200
--- tests/test_db/test_db.py
+++
@@ -1,8 +0,0 @@
-from setup.sql_setup.sql_setup import get_db_session
-from setup.mongo_setup.mongo_setup import get_mongo_client, ObjectId
-
-def test_create_mongo_session():
-    client = get_mongo_client()
-    assert client
-    client.close()
-    assert ObjectId
